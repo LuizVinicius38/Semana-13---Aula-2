cd0dfc63f56882850cb8c2e77df350f205b827f1
LuizVinicius38 <89944844+LuizVinicius38@users.noreply.github.com>
2021-12-13 16:48:20 -0300

    Add files via upload

---
+++ Sem-13-T2-Q3.py
@@ -0,0 +1,12 @@
+lista1 = []
+lista2 = []
+lista3 = []
+for v in range(20):
+    lista1.append(int(input("")))
+for i in range(20):
+    lista2.append(int(input("")))
+for n in range(20):
+    lista3.append(lista1[n] + lista2[n])
+print(lista1)
+print(lista2)
+print(lista3)
